6522369f0d1d845ab4842f66928ebb2911952fe2
eko <eko@localhost.localdomain>
2020-09-01 16:11:32 +0700

    add routes and passport

--- config/passport.js
+++ config/passport.js
@@ -33,8 +33,46 @@ module.exports = function(passport) {
                     return done(err);
                 if(rows.length){
                     return done(null, false, req.flash('signupMessage', 'that is already taken'));
+                }else{
+                    var newUserMysql = {
+                        username: username,
+                        password: bcrypt.hashSync(password, null, null)
+                    };
+
+                    var insertQuery = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+                    connection.query(insertQuery, [newUserMysql.username, newUserMysql.password],
+                        function(err, rows){
+                            newUserMysql.id = rows.insertId;
+
+                            return done(null, newUserMysql);
+
+                        });
                 }
-            })
+            });
         })
-    )
-}
+    );
+    
+passport.use(
+    'local-login',
+    new LocalStrategy({
+        usernameField : 'username',
+        passwordField : 'password',
+        passReqToCallback: true
+    },
+    function(req, username, password, done){
+            connection.query("SELECT * FROM users WHERE username = ? ", [username],
+            function(err, rows){
+                if(err)
+                    return done(err);
+                if(!rows.length){
+                    return done(null, false, req.flash('loginMessage', 'No User Found'));
+                }
+                if(!bcrypt.compareSync(password, rows[0].password))
+                return done(null, false, req.flash('loginMessage', 'Wrong Password'));
+
+                return done(null, rows[0]);
+            });
+        })
+    );
+};
